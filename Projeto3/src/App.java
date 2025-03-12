a9ce9968b8060ab6682820d9dc9ee8b590755998
eduardaFloripo <eduardafloripoxavier@gmail.com>
2025-03-12 00:25:14 +0000

    Aualização aula4

--- Projeto3/src/App.java
+++ Projeto3/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        SSystem.out.println("Oi, professor!");
+        System.out.println("Oi, professor!");
+        System.out.println("Atualizando");
+
     }
 }
